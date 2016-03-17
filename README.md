db381ed5bf761bf15135656f1ec970370e21f251
Vladimir Prus <vladimir.prus@gmail.com>
2016-03-17 18:44:09 +0300

    Facebook login implementation.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# android-little-program
+# Anroid Little Program: Facebook Login
+
+This branch of the little program implements minimal Facebook login functionality. To try it,
+you'd need to create a new Facebook application, at https://developers.facebook.com/quickstarts/?platform=android
+
+- You'll likely need to change package name
+- You will definitely need to provide your own certificate fingerprint
+- Along the way, you'll be informed of the Facebook app id. Modify strings.xml accordingly.
+- You can ignore other instructions to modify application code.
+
+
--- app/src/main/java/com/vladimirprus/littleprogram/MainActivity.java
+++ app/src/main/java/com/vladimirprus/littleprogram/MainActivity.java
@@ -1,13 +1,68 @@
 package com.vladimirprus.littleprogram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(MainActivity.this, "Successfully logged in", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Login was cancelled", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(MainActivity.this, "Error while logging in", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
+    }
+
 }
