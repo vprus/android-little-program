90f36e9e6424d11b87e038197cb34acf510dcfbe
Vladimir Prus <vladimir.prus@gmail.com>
2016-03-18 13:34:50 +0300

    Test getting Facebook user data via HTTP.

--- app/src/main/java/com/vladimirprus/littleprogram/MainActivity.java
+++ app/src/main/java/com/vladimirprus/littleprogram/MainActivity.java
@@ -3,8 +3,14 @@ package com.vladimirprus.littleprogram;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -14,7 +20,11 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getName();
 
     CallbackManager callbackManager;
 
@@ -28,11 +38,15 @@ public class MainActivity extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
         loginButton.registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         Toast.makeText(MainActivity.this, "Successfully logged in", Toast.LENGTH_LONG).show();
+
+                        AccessToken token = AccessToken.getCurrentAccessToken();
+                        getFacebookProfile(token);
                     }
 
                     @Override
@@ -45,6 +59,30 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "Error while logging in", Toast.LENGTH_LONG).show();
                     }
                 });
+    }
+
+    // Obtains user information given Facebook access token.
+    // We actually can get everything from SDK, but just for
+    // education, let's make sure HTTP requests also work.
+    private void getFacebookProfile(AccessToken token) {
+        String url = "https://graph.facebook.com/v2.5/me?fields=id,name,first_name,last_name,birthday,email,age_range,picture&access_token=" + token.getToken();
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e(TAG, "Got response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, error.getMessage());
+                    }
+                });
+
+        NetworkManager.getInstance(MainActivity.this).addToRequestQueue(jsObjRequest);
     }
 
     @Override
---
+++ app/src/main/java/com/vladimirprus/littleprogram/NetworkManager.java
@@ -0,0 +1,37 @@
+package com.vladimirprus.littleprogram;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class NetworkManager {
+    private static NetworkManager mInstance;
+
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    private NetworkManager(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized NetworkManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new NetworkManager(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
